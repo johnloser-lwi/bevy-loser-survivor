d5e223029e68bc6c3902f4edf6a848bf3dfe43a8
johnloser <66107110+johnloser-lwi@users.noreply.github.com>
2024-04-24 13:07:26 -0700

    add game plugin

---
+++ src/game/mod.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::*;
+
+mod systems;
+
+pub struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut App) {
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 
 use bevy::prelude::*;
+use game::*;
+
+
+mod game;
+
 
 fn main() {
     App::new()
     .insert_state(AppState::Game)
     .insert_state(GameState::Running)
     .add_plugins(DefaultPlugins)
+    
+
+    // Custom Plugins
+    .add_plugins(GamePlugin)
+
     .run();
 }
 
