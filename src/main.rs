a21e2e76ae7c7087d1714e1d60d68b04dca558a0
johnloser <66107110+johnloser-lwi@users.noreply.github.com>
2024-04-24 13:02:34 -0700

    add global states

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,24 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+    .insert_state(AppState::Game)
+    .insert_state(GameState::Running)
     .add_plugins(DefaultPlugins)
     .run();
+}
+
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum GameState {
+    #[default]
+    Running,
+    Paused
 }
