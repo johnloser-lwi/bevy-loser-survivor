2af24eb277e9a3e791dfcbbbea85cdb58cf9025a
johnloser <66107110+johnloser-lwi@users.noreply.github.com>
2024-04-29 13:20:44 -0700

    add web icon and build script

---
+++ web_build.ps1
@@ -0,0 +1,2 @@
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --no-typescript --target web --out-dir ./out/ --out-name "loser-survivor" ./target/wasm32-unknown-unknown/release/bevy-loser-survivor.wasm
